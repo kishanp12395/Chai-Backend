6103ed53e160850440404e8323bf3fc008eeeef8
kishanp12395 <kkp12395@gmail.com>
2025-03-22 19:16:22 +0530

    generate utils

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv';
-import express from 'express';
-import mongoose, { connect } from 'mongoose';
 import connectDB from './db/db.js';
 
 dotenv.config({path: './env'});
+import { app } from './app.js';
 
+const port = process.env.PORT || 3000;
 
-
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+}); 
