8c815929ffbbc97af1a141cb064b4de4d9143ade
kishanp12395 <kkp12395@gmail.com>
2025-03-23 14:28:50 +0530

    user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema({
+    videoFile: {
+        type: String, // cloudinary url
+        required: [true, "Video file is required"],
+    },
+    thumbnail: {
+        type: String, // cloudinary url
+        required: [true, "Thumbnail is required"],
+    },
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+},{ timestamps: true });
+
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+const Video = mongoose.model("Video", videoSchema);
+
+export default Video;
